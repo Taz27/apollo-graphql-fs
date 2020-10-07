cef668283fb848e4d63009e459ab0c913bbde5a9
Taran Mand <tsmand@yahoo.com>
2020-10-08 03:58:38 +1100

    add mutation schema and resolvers

--- my-first-gql/resolvers.js
+++ my-first-gql/resolvers.js
@@ -18,17 +18,35 @@ const Query = {
         //console.log(context);
         //console.log(info);
         return db.students.get(args.id);
-    }
+    },
+    setFavColor: (root, args) => `Your favorite fucking color is ${args.color}` 
 };
 
-//for each single student object returned,resolver is invoked
-
+//for each single Student object returned,resolver is invoked
 const Student = {
-    fullName: (root, args, context, info) => `${root.firstName} ${root.lastName}`
+    fullName: (root, args, context, info) => `${root.firstName} ${root.lastName}`,
+    college: (root) => db.colleges.get(root.collegeId)
 };
 
+//for each single User object returned,resolver is invoked
 const User = {
     nickname: (root, args, context, info) => `${root.company.name}`
 };
 
-module.exports = {Query, Student, User}
+const Mutation = {
+    createStudent: (root, args, context, info) => db.students.create({firstName: args.firstName, lastName: args.lastName, collegeId: args.collegeId}),
+    addStudent_returns_object: (root, args, context, info) => {
+       let id = db.students.create({firstName: args.firstName, lastName: args.lastName, collegeId: args.collegeId});
+       return db.students.get(id);
+    },
+    delStudent: (root, args, context, info) => {
+        db.students.delete(args.sid);
+        return "SUCCESS";
+    },
+    updateStudent: (root, args) => {
+        db.students.update(args.stud);
+        return "SUCCESSFULLY UPDATED";
+    }
+};
+
+module.exports = {Query, Student, User, Mutation}
