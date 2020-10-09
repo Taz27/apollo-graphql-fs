f7b254ba2053d55ab2bd58ef3baabedcf8092db3
Taran Mand <tsmand@yahoo.com>
2020-10-09 13:03:09 +1100

    add other route handlers to server

--- my-first-gql/apollo-express-server/server.js
+++ my-first-gql/apollo-express-server/server.js
@@ -25,6 +25,19 @@ const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
 app.use('/graphql',graphqlExpress({schema}))
 app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
 
+//root route
+app.get("/", (req, res) => {
+   res.set('Content-Type', 'text/html');
+   res.write('<h2>Welcome to my GraphQL Server!</h2>');
+   res.write(`<h3>You can send query requests to path <span style="color: blue;">/graphql</span></h3>`);
+   res.write(`<h3>You can access <em>GraphiQL IDE</em> at path <span style="color: blue;">/graphiql</span></h3>`);
+   res.end();
+});
+
+app.get("*", (req, res) => {
+   res.status(404).send(`<h3>404 - Page Not Found!</h3>`);
+});
+
 //registering port
 app.listen(
    port, () => console.info(
